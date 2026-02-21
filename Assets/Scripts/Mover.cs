b421203badf9a02c7f6e50764c7b93b6902204f6
peri.gold81@gmail.com <peri.gold81@gmail.com>
2026-02-21 15:51:34 +0200

    some colliders and textures

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float _movemnetSpeed=1f;
+    float _xValue,_yValue,_zValue;
+    
     void Start()
     {
-        
+
+     //   transform.Translate(transform.position.x+1,transform.position.y,transform.position.z+1);
     }
 
     // Update is called once per frame
     void Update()
     {
+        float delta = Time.deltaTime;  
+        _xValue=Input.GetAxis("Horizontal") * delta * _movemnetSpeed;
+        _yValue=0;
+        _zValue=Input.GetAxis("Vertical") * delta * _movemnetSpeed ;
+        transform.Translate(_xValue,_yValue,_zValue);
         
     }
 }
